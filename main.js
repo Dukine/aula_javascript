e996f992096857b675a50e722aa5f7d5651f4d13
Dukine <gabrielwrnck@gmail.com>
2021-05-24 05:16:02 -0300

    Feito alguns testes com array

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 
-var nome = "Gabriel Werneck";
-var idade = 26;
-var idade2 = 10;
-var frase= "Pizza é top"
+
+
+//var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"},{nome:"laranja", cor:"laranja"}];
+//console.log(frutas)
+/*var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta)*/
+//var lista = ["maça", "pêra","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" & "));
+
+//var nome = "Gabriel Werneck";
+//var idade = 26;
+//var idade2 = 10;
+//var frase= "Pizza é top"
 //alert(nome + " tem " + idade + " anos");
 //alert(idade + idade2);
 //alert (frase.replace("Pizza", "Lasanha"));
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.toUpperCase());
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toUpperCase());
