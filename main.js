ace016f8639f51065cc5982d70e5b009aaf31f55
Dukine <gabrielwrnck@gmail.com>
2021-05-24 04:58:20 -0300

    Criado o arquivo JavaScript e feito alguns testes

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var nome = "Gabriel Werneck";
+var idade = 26;
+var idade2 = 10;
+var frase= "Pizza é top"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//alert (frase.replace("Pizza", "Lasanha"));
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
