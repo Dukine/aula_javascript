4027615ca87d9d39c4f2857868cb1f349dce062e
Dukine <gabrielwrnck@gmail.com>
2021-05-24 06:08:13 -0300

    Feito exercícios de condicionais e laços de repetição

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
 
 
 
+
+/*
+var d = new Date();
+alert(d.getDay()+1);
+alert(d.getHours() + ":" + d.getMinutes());
+*/
+
+/*
+for(var count=0; count<=5; count++){
+console.log(count);
+}
+*/
+
+/*
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+}
+*/
+
+/* 
+var idade = prompt ("Qual sua idade?");
+if (idade >= 18){
+    alert("Você é maior de idade!");
+}else{
+    var diferenca = 18 - idade;
+    if (diferenca == 1){
+        alert("Você é menor de idade! Volte em 1 ano!")
+    }else{
+        alert("Você é menor de idade! Volte em " + diferenca + " anos!")
+    }
+} 
+*/
+
 //var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"},{nome:"laranja", cor:"laranja"}];
 //console.log(frutas)
-/*var fruta = {nome:"maçã", cor:"vermelha"};
-console.log(fruta)*/
+/*
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta)
+*/
 //var lista = ["maça", "pêra","laranja"];
 //lista.push("uva");
 //lista.pop();
